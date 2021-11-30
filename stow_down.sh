0a99119b7380b0b21eb47c4a2ab990a85157c542
Lily Hughes-Robinson <oaguy1@gmail.com>
2021-11-30 10:20:23 -0500

    Cleanup .config links, create scripts for (un)loading

---
+++ stow_down.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+stow -D bash
+stow -D emacs
+stow -D -t ~/.config fish
+stow -D -t ~/.config git
+stow -D tmux
+stow -D vim
---
+++ stow_up.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+stow bash
+stow emacs
+stow -t ~/.config fish
+stow -t ~/.config git
+stow tmux
+stow vim
