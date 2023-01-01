c725d6135d90e620c417ab2db11b03e030e6b7a3
Lily Hughes-Robinson <oaguy1@gmail.com>
2022-12-31 19:12:27 -0500

    Add stub for neovim

--- stow_down.sh
+++ stow_down.sh
@@ -4,5 +4,6 @@ stow -D bash
 stow -D emacs
 stow -D -t ~/.config fish
 stow -D -t ~/.config git
+stow -D -t ~/.config nvim
 stow -D tmux
 stow -D vim
--- stow_up.sh
+++ stow_up.sh
@@ -4,5 +4,6 @@ stow bash
 stow emacs
 stow -t ~/.config fish
 stow -t ~/.config git
+stow -t ~/.config nvim
 stow tmux
 stow vim
